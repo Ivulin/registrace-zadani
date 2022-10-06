1bbef57795a2bc3f0a00996ee5443fb063214b55
Ivulin <i.kreslova@gmail.com>
2022-10-06 19:43:24 +0200

    Oprava chyb

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -21,11 +21,7 @@ const Registration = () => {
 
     const [errorMessage, setErrorMessage] = useState(null);
 
-    useEffect(()=>{
-        console.log(usr);
-      },[usr]);
-
-    const onhandleChange = (id,value) => {
+    const handleChange = (id,value) => {
         let newUser= { ...usr,[id]: value };
         setUsr(newUser);         
       };
@@ -34,17 +30,11 @@ const Registration = () => {
         event.preventDefault();
         if(emailValidation(usr.email) && passwordValidation(usr.password,usr.passwordConfirm))
         {      
-            let usrName = usr.username ? usr.username : getUser(usr.email);
-            onhandleChange('username', usrName);
+            let usrName = usr.username ? usr.username : computeUser(usr.email);
+            handleChange('username', usrName);
         }
-        else{
-            setUsr({
-                username: '',
-                email: '',
-                password: '',
-                passwordConfirm: '',
-            });
-        }
+
+        console.log(usr);
     }
 
     const emailValidation = (email) => {
@@ -63,7 +53,7 @@ const Registration = () => {
     return true;
    }
 
-   const getUser = (email) => {
+   const computeUser = (email) => {
         return email.substring(0,email.indexOf('@'));
    }
 
@@ -72,10 +62,10 @@ const Registration = () => {
   return (   
     <div className="registration_form">       
         <form onSubmit={handleSubmit}>
-            <TextBox id="email" title={INITIAL_LIST.email} value={usr} onChangeHandler={onhandleChange}/>
-            <TextBox id="username" title={INITIAL_LIST.username} value={usr} onChangeHandler={onhandleChange}/>
-            <TextBox id="password" title={INITIAL_LIST.password} value={usr} onChangeHandler={onhandleChange}/>
-            <TextBox id="passwordConfirm" title={INITIAL_LIST.passwordConfirm} value={usr} onChangeHandler={onhandleChange}/>
+            <TextBox id="email" title={INITIAL_LIST.email} value={usr} onChangeHandler={handleChange}/>
+            <TextBox id="username" title={INITIAL_LIST.username} value={usr} onChangeHandler={handleChange}/>
+            <TextBox id="password" title={INITIAL_LIST.password} value={usr} onChangeHandler={handleChange}/>
+            <TextBox id="passwordConfirm" title={INITIAL_LIST.passwordConfirm} value={usr} onChangeHandler={handleChange}/>
             {errorMessage ? <div>{errorMessage}</div> : null}
             <button className='submit_btn' type="submit">REGISTRATION</button>
         </form>
--- src/components/TextBox/index.jsx
+++ src/components/TextBox/index.jsx
@@ -2,39 +2,15 @@ import React, {useState, useEffect} from 'react';
 import './style.css';
 
 const TextBox = ({id, title, value, onChangeHandler}) => {
-    const [text, setText] = useState(value[id] == "" ? title : value[id]);
-
-    const onFocusHanlder = (e)=>{
-      console.log(e.target.value);
-        if(e.target.value == title)
-        {
-          setText('');
-          onChangeHandler(e.target.id,'')
-        }
-          
-    };
-
-    const onBlurHanlder = (e)=>{
-      if(e.target.value == "")
-      {
-        setText(title);
-        onChangeHandler(e.target.id,'');
-      }
-    };
-
-    const onChangeClick = (e) =>{
-      if(e.target.value != title && e.target.value!="")
-        onChangeHandler(id,e.target.value)
-    }
+    const [text, setText] = useState("");
 
     useEffect(()=>{
-      if(text!=value[id])
-        setText(value[id] == "" ? title : value[id]);
+        setText(value[id]);
     },[value]);
 
   return (       
         <div>
-          <input className="input_text" id={id} name={id} type="text" value={text} onChange={onChangeClick} onFocus={onFocusHanlder} onBlur={onBlurHanlder}/>
+          <input className="input_text" id={id} name={id} type="text" placeholder={title} value={text} onChange={(e)=>onChangeHandler(e.target.id,e.target.value)} />
         </div>
   );
 };
